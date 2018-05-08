566ce3cc7a3cbe680fbfe54afa64557a1d9ea2e4
Samir Akre <sakre@ucdavis.edu>
2018-05-07 18:45:01 -0700

    bnocsv

---
+++ bno/bno_csv.py
@@ -0,0 +1,76 @@
+import smbus,time,csv,logging
+import sys, getopt 
+from Adafruit_BNO055 import BNO055
+
+
+
+
+bno1= BNO055.BNO055(address=0x28)
+bno2= BNO055.BNO055(address=0x29)
+
+# Enable verbose debug logging if -v is passed as a parameter.
+if len(sys.argv) == 2 and sys.argv[1].lower() == '-v':
+    logging.basicConfig(level=logging.DEBUG)
+
+# Initialize the BNO055 and stop if something went wrong.
+if not (bno1.begin() and bno2.begin()):
+    raise RuntimeError('Failed to initialize BNO055! Is the sensor connected?')
+
+# Print system status and self test result.
+status1, self_test1, error2 = bno1.get_system_status()
+status2, self_test2, error2 = bno2.get_system_status()
+
+print('System status 1: {0}'.format(status1))
+print('Self test result 1 (0x0F is normal): 0x{0:02X}'.format(self_test1))
+
+
+# Print out an error if system status is in error mode.
+if status1 == 0x01:
+    print('System 1 error: {0}'.format(error1))
+    print('See datasheet section 4.3.59 for the meaning.')
+
+print('System status 2: {0}'.format(status2))
+print('Self test result 2 (0x0F is normal): 0x{0:02X}'.format(self_test2))
+
+if status2 == 0x01:
+    print('System 2 error: {0}'.format(error2))
+    print('See datasheet section 4.3.59 for the meaning.')
+
+
+
+sw1, bl1, accel1, mag1, gyro1 = bno1.get_revision()
+sw2, bl2, accel2, mag2, gyro2 = bno2.get_revision()
+
+print('Reading BNO055 data, press Ctrl-C to quit...')
+
+with open ('BNO_testData.csv','wb') as csvfile: writer= csv.writer(csvfile)
+    header=['time (ms)','x','y','z','w','x2','y2','z2','w2']
+    writer.writerow(header)
+    t0=int(round(time.time() * 1000))
+    for i in range(10000):
+        # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
+        sys, gyro, accel, mag = bno1.get_calibration_status()
+        print('SysCal:{0:0.2F}'.format(sys))
+        sys, gyro, accel, mag = bno2.get_calibration_status()
+
+        # Print everything out.
+        print('bno1')
+        x1,y1,z1,w1 = bno1.read_quaternion()
+        print('x={0:0.2F}, y={1:0.2F},z={2:0.2F},w={3:0.2F}'.format(x,y,z,w))
+
+        print('bno2')
+        x2,y2,z2,w2 = bno2.read_quaternion()
+        print('x={0:0.2F}, y={1:0.2F},z={2:0.2F},w={3:0.2F}'.format(x,y,z,w))
+        
+        
+        t=[int(round(time.time() * 1000))-t0]
+        data=[t,x1,y1,z1,w1,x2,y2,z2,w2]
+        writer.writerow(data)
+        csvfile.flush()
+         
+
+
+
+
+
+
