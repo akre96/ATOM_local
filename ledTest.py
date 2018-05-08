87de594f261a2e6c3e6853d86203e0c86951865b
Samir Akre <sakre@ucdavis.edu>
2018-05-07 18:09:55 -0700

    testLED

---
+++ ledTest.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+import time
+
+
+
+# Pin Def
+
+R= 17
+G=27
+B=22
+
+
+# SETUP
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(R,GPIO.OUT)
+GPIO.setup(G,GPIO.OUT)
+GPIO.setup(B,GPIO.OUT)
+
+
+ i = range(5)
+
+ for i:
+    GPIO.output(R,GPIO.HIGH)
+    GPIO.output(G,GPIO.HIGH)
+    GPIO.output(B,GPIO.HIGH)
+
+    time.sleep(1)
+
+    GPIO.output(R,GPIO.LOW)
+    GPIO.output(G,GPIO.LOW)
+    GPIO.output(B,GPIO.LOW)
+
+    time.sleep(1)
+
+
